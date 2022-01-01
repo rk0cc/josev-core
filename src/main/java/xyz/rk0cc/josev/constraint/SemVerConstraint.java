1865b52ee5ee0a02f6d8f0f38f65f64d4441d047
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-01 13:41:36 +0800

    Add parser

--- src/main/java/xyz/rk0cc/josev/constraint/SemVerConstraint.java
+++ src/main/java/xyz/rk0cc/josev/constraint/SemVerConstraint.java
@@ -3,6 +3,9 @@ package xyz.rk0cc.josev.constraint;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public abstract class SemVerConstraint<E extends ConstraintPattern<? extends Enum<?>>> implements Serializable {
     private final String rawConstraint;
@@ -51,4 +54,44 @@ public abstract class SemVerConstraint<E extends ConstraintPattern<? extends Enu
     }
 
     protected abstract boolean isValidConstraint();
+
+    /**
+     * Parsing version constraint to specific class which inherited from {@link SemVerConstraint}.
+     *
+     * @param constraintClass Which {@link Class} will be used for parsing <code>versionConstraint</code>.
+     * @param versionConstraint A {@link String} of version constraint.
+     * @param <C> Specified which {@link Class} will be used that extended with {@link SemVerConstraint}.
+     *
+     * @apiNote To use this static method, the target of <code>constraintClass</code> must be a non-abstract class with
+     *          <code>parse(String)</code> provided.
+     *
+     * @return An object of {@link C} which representing information of {@link SemVerConstraint}.
+     *
+     * @throws UnsupportedOperationException If the <code>constraintClass</code> can not be used as return type of
+     *                                       {@link SemVerConstraint} which does not meet requirement of API note.
+     */
+    @SuppressWarnings("unchecked")
+    public static <C extends SemVerConstraint<? extends ConstraintPattern<? extends Enum<?>>>> C parse(
+            @Nonnull Class<C> constraintClass,
+            @Nullable String versionConstraint
+    ) {
+        try {
+            final Method parser = constraintClass.getMethod("parse", String.class);
+            final int cModifier = constraintClass.getModifiers();
+            final int cPModifier = parser.getModifiers();
+
+            assert  Modifier.isPublic(cModifier)
+                    && !Modifier.isAbstract(cModifier)
+                    && !Modifier.isInterface(cModifier)
+                    && Modifier.isPublic(cPModifier)
+                    && Modifier.isStatic(cPModifier);
+
+            return (C) parser.invoke(null, versionConstraint);
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException | AssertionError e) {
+            throw new UnsupportedOperationException(
+                    "'" + constraintClass.getName() + "' can not become a parser or no parser found in this class.",
+                    e
+            );
+        }
+    }
 }
