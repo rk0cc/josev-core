b2f52a93fd8198c748649e0b7e01f1b693f2a23b
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-01 13:01:21 +0800

    Impl condition

--- src/main/java/xyz/rk0cc/josev/constraint/ConstraintPattern.java
+++ src/main/java/xyz/rk0cc/josev/constraint/ConstraintPattern.java
@@ -1,8 +1,20 @@
 package xyz.rk0cc.josev.constraint;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.Serializable;
 import java.util.regex.Pattern;
 
 public interface ConstraintPattern<E extends Enum<? extends ConstraintPattern<E>>> extends Serializable {
-    Pattern getConstraintPattern();
+    @Nonnull
+    Pattern constraintPattern();
+
+    default boolean acceptParseNull() {
+        return false;
+    }
+
+    default boolean isValidConstraintMethods(@Nullable String versionConstraint) {
+        if (versionConstraint == null) return acceptParseNull();
+        return constraintPattern().matcher(versionConstraint).matches();
+    }
 }
