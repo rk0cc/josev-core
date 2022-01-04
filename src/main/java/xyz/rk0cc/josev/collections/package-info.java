c21fb0f178b413966d35fdf7abe2d8e3a611a3b4
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-04 22:12:59 +0800

    Document collection

--- src/main/java/xyz/rk0cc/josev/collections/package-info.java
+++ src/main/java/xyz/rk0cc/josev/collections/package-info.java
@@ -1,1 +1,7 @@
+/**
+ * A package of {@link java.util.Collection} with additional feature with {@link xyz.rk0cc.josev} and
+ * {@link xyz.rk0cc.josev.constraint.SemVerConstraint Semver constraint}.
+ *
+ * @since 1.0.0
+ */
 package xyz.rk0cc.josev.collections;
