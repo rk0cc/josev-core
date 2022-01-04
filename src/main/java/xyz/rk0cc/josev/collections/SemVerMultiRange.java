3e8df7f5e77f0bfa3e83bb0f25cd02bf517624d1
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-04 22:10:19 +0800

    Check is not empty before unmodify

--- src/main/java/xyz/rk0cc/josev/collections/SemVerMultiRange.java
+++ src/main/java/xyz/rk0cc/josev/collections/SemVerMultiRange.java
@@ -24,50 +24,78 @@ public final class SemVerMultiRange implements Set<NonnullSemVerRange>, SemVerDe
     /**
      * A {@link Set} of applied {@link SemVer}.
      */
-    private final HashSet<NonnullSemVerRange> rangeSets;
+    private final Set<NonnullSemVerRange> rangeSets;
     private final boolean unmodifiable;
 
+    /**
+     * Create an empty definitions of {@link SemVerMultiRange}.
+     */
     public SemVerMultiRange() {
         this.rangeSets = new HashSet<>();
         this.unmodifiable = false;
     }
 
+    /**
+     * Create new {@link SemVerMultiRange} from exist data.
+     *
+     * @param origin Original data of {@link SemVerMultiRange}
+     * @param unmodifiable Apply origin's {@link SemVerMultiRange#rangeSets} via {@link Set#copyOf(Collection)} to
+     *                     disable modification in this instance.
+     */
     private SemVerMultiRange(@Nonnull SemVerMultiRange origin, boolean unmodifiable) {
-        HashSet<NonnullSemVerRange> originRS = new HashSet<>(origin.rangeSets);
+        Set<NonnullSemVerRange> originRS = new HashSet<>(origin.rangeSets);
         this.rangeSets = unmodifiable
-                ? (HashSet<NonnullSemVerRange>) Set.copyOf(originRS)
-                :originRS;
+                ? Set.copyOf(originRS)
+                : originRS;
         this.unmodifiable = unmodifiable;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Nonnegative
     @Override
     public int size() {
         return rangeSets.size();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isEmpty() {
         return rangeSets.isEmpty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean contains(Object o) {
         return rangeSets.contains(o);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Nonnull
     @Override
     public Iterator<NonnullSemVerRange> iterator() {
         return rangeSets.iterator();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Nonnull
     @Override
     public Object[] toArray() {
         return rangeSets.toArray();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @SuppressWarnings("SuspiciousToArrayCall")
     @Nonnull
     @Override
@@ -75,77 +103,128 @@ public final class SemVerMultiRange implements Set<NonnullSemVerRange>, SemVerDe
         return rangeSets.toArray(a);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean add(@Nonnull NonnullSemVerRange nonnullSemVerRange) {
         return rangeSets.add(nonnullSemVerRange);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean remove(Object o) {
         return rangeSets.remove(o);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean containsAll(@Nonnull Collection<?> c) {
         return rangeSets.containsAll(c);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean addAll(@Nonnull Collection<? extends NonnullSemVerRange> c) {
         return rangeSets.addAll(c);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean retainAll(@Nonnull Collection<?> c) {
         return rangeSets.retainAll(c);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean removeAll(@Nonnull Collection<?> c) {
         return rangeSets.removeAll(c);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void clear() {
         rangeSets.clear();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Spliterator<NonnullSemVerRange> spliterator() {
         return rangeSets.spliterator();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @SuppressWarnings("SuspiciousToArrayCall")
     @Override
     public <T> T[] toArray(IntFunction<T[]> generator) {
         return rangeSets.toArray(generator);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean removeIf(Predicate<? super NonnullSemVerRange> filter) {
         return rangeSets.removeIf(filter);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Stream<NonnullSemVerRange> stream() {
         return rangeSets.stream();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Stream<NonnullSemVerRange> parallelStream() {
         return rangeSets.parallelStream();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void forEach(Consumer<? super NonnullSemVerRange> action) {
         rangeSets.forEach(action);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return <code>true</code> if this {@link SemVer} is inside the range.
+     */
     @Override
     public boolean isInRange(@Nonnull SemVer semVer) {
         return stream().anyMatch(msv -> msv.isInRange(semVer));
     }
 
+    /**
+     * Determine this {@link SemVerMultiRange} is not allowed {@link #add(NonnullSemVerRange)}, {@link #remove(Object)}
+     * and any applying new {@link SemVerRange} to changing state of {@link Set}. It only returns <code>true</code> by
+     * parsing {@link #disableModification(SemVerMultiRange)}.
+     *
+     * @return Check this {@link SemVerMultiRange} disabled modification.
+     */
     public boolean isUnmodifiable() {
         return unmodifiable;
     }
@@ -153,7 +232,7 @@ public final class SemVerMultiRange implements Set<NonnullSemVerRange>, SemVerDe
     /**
      * Perform shallow copy of multi range.
      *
-     * @return Cloned {@link Object} of {@link SemVerMultiRange}
+     * @return Cloned {@link Object} of {@link SemVerMultiRange}.
      *
      * @see HashSet#clone()
      */
@@ -162,7 +241,7 @@ public final class SemVerMultiRange implements Set<NonnullSemVerRange>, SemVerDe
         try {
             return super.clone();
         } catch (CloneNotSupportedException e) {
-            return new SemVerMultiRange(this, false);
+            return new SemVerMultiRange(this, unmodifiable);
         }
     }
 
@@ -177,8 +256,20 @@ public final class SemVerMultiRange implements Set<NonnullSemVerRange>, SemVerDe
         return '[' + Joiner.on(", ").join(rangeSets) + ']';
     }
 
+    /**
+     * Generate a {@link SemVerMultiRange} that to generate unmodifiable <code>multiRange</code>.
+     *
+     * @param multiRange An {@link SemVerMultiRange} that want to disable modification.
+     *
+     * @return A new instance of {@link SemVerMultiRange} which no longer edit {@link Set} data;
+     *
+     * @throws IllegalStateException If <code>multiRange</code> {@link SemVerMultiRange#isEmpty()} return
+     *                               <code>true</code> which means no constraint definitions is set then disable
+     *                               modification.
+     */
     @Nonnull
     public static SemVerMultiRange disableModification(@Nonnull SemVerMultiRange multiRange) {
+        if (multiRange.isEmpty()) throw new IllegalStateException("Applying empty set of multi range is forbidden");
         return new SemVerMultiRange(multiRange, true);
     }
 }
