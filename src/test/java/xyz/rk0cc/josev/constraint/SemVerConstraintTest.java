a7676e13a93b5af4829e2ae0842e613ce8cd38f0
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-09 16:24:18 +0800

    Add test task

--- src/test/java/xyz/rk0cc/josev/constraint/SemVerConstraintTest.java
+++ src/test/java/xyz/rk0cc/josev/constraint/SemVerConstraintTest.java
@@ -2,6 +2,10 @@ package xyz.rk0cc.josev.constraint;
 
 import org.junit.jupiter.api.*;
 import xyz.rk0cc.josev.NonStandardSemVerException;
+import xyz.rk0cc.josev.SemVer;
+import xyz.rk0cc.josev.collections.SemVerMultipleRange;
+
+import javax.annotation.Nonnull;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,6 +44,37 @@ final class SemVerConstraintTest {
             assertFalse(sc.isInRange("0.9.0"));
             assertFalse(sc.isInRange("2.0.0"));
             assertFalse(sc.isInRange("2.0.0-alpha"));
+        } catch (NonStandardSemVerException e) {
+            fail(e);
+        }
+    }
+
+    @DisplayName("Test multiple range")
+    @Order(4)
+    @Test
+    void testMultipleRange() {
+        SemVerMultipleRange<SampleConstraint> mr = new SemVerMultipleRange<>() {
+            @Override
+            public boolean isInRange(@Nonnull SemVer semVer) {
+                return this.stream().anyMatch(s -> s.isInRange(semVer));
+            }
+        };
+
+        mr.add(SampleConstraint.parse(">=1.0.0 <2.0.0"));
+        mr.add(SampleConstraint.parse(">=3.0.0 <4.0.0"));
+
+        try {
+            assertTrue(mr.isInRange("1.0.0"));
+            assertFalse(mr.isInRange("1.0.0-rc.1"));
+            assertTrue(mr.isInRange("1.0.0+1"));
+            assertTrue(mr.isInRange("1.9.0"));
+            assertTrue(mr.isInRange("1.0.1-beta"));
+            assertFalse(mr.isInRange("0.9.0"));
+            assertFalse(mr.isInRange("2.0.0"));
+            assertFalse(mr.isInRange("2.0.0-alpha"));
+            assertTrue(mr.isInRange("3.0.0"));
+            assertTrue(mr.isInRange("3.1.0"));
+            assertFalse(mr.isInRange("4.0.0"));
         } catch (NonStandardSemVerException e) {
             fail(e);
         }
